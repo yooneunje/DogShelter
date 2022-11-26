377a9d005675b4b7781d9c8cb782500ad1b19989
yooneunje <87642401+yooneunje@users.noreply.github.com>
2022-11-26 14:36:51 +0900

    project setting

---
+++ together/src/test/java/pet/dogshelter/together/TogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package pet.dogshelter.together;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
